7ddb4eedd966b37d68dcad5b3d2af3576509c1b2
Adalid Villanueva <avillanueva047@gmail.com>
2020-02-06 19:02:43 +0100

    nueva clase delivered

---
+++ src/app/models/delivered.ts
@@ -0,0 +1,11 @@
+export class Delivered{
+  user_id: number;
+  delivery_name: string;
+  deliver_name: string;
+  client_name: string;
+  client_email: string;
+  client_direction: string;
+  client_phone: string;
+  latitude: number;
+  longitude: number;
+}
